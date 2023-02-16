c7a4cd4b476ebc3f387eebaeb037db81a5e07747
DaniilPetrov <dany_petrov@icloud.com>
2023-02-16 12:41:35 +0300

    Initial Commit

---
+++ myShop/myShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  myShopApp.swift
+//  myShop
+//
+//  Created by Даниил Петров on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct myShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
